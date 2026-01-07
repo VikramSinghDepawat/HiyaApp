865f5d3983c91684d89dc48073d99ce360b14c74
vikramsingh <vc7662@gmail.com>
2026-01-07 17:12:16 +0530

    Initial Commit

---
+++ Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by Vikram Singh Depawat on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
