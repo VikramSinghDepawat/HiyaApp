3298a894afd194bb1f80dfb329f5e0bf4c4493c9
vikramsingh <vc7662@gmail.com>
2026-01-07 18:37:00 +0530

    Add new variable of Language Model Session Add spacer top and bottom vstack to show text in center add view modifier to enhance text view Add Button with glass effect and add task to handle session response

--- Hiya/ContentView.swift
+++ Hiya/ContentView.swift
@@ -9,14 +9,21 @@ import SwiftUI
 import FoundationModels
 
 struct ContentView: View {
-    
     private var largeLanguageModel = SystemLanguageModel.default
+    private var session = LanguageModelSession()
+
     @State private var response = ""
+
     var body: some View {
         VStack {
+            Spacer()
+            
             switch largeLanguageModel.availability {
             case .available:
-                Text("Available")
+                Text(response)
+                    .multilineTextAlignment(.center)
+                    .font(.largeTitle)
+                    .bold()
             case .unavailable(.deviceNotEligible):
                 Text("Your device is not eligible for Apple Intelligence.")
             case .unavailable(.appleIntelligenceNotEnabled):
@@ -26,8 +33,31 @@ struct ContentView: View {
             case .unavailable:
                 Text("Unavailable")
             }
+            
+            Spacer()
+            
+            Button {
+                Task {
+                    let prompt = "Say hi in a fun way."
+                    
+                    do {
+                        let reply = try await session.respond(to: prompt)
+                        response = reply.content
+                    } catch {
+                        response = "Failed to get response: \(error.localizedDescription)"
+                    }
+                }
+            } label: {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding()
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonSizing(.flexible)
+            .glassEffect(.regular.interactive())
         }
         .padding()
+        .tint(.purple)
     }
 }
 
