de80e24f574fc094c9f313c8f5b441524b02ad3e
vikramsingh <vc7662@gmail.com>
2026-01-07 18:24:07 +0530

    change uilanchscreen to image name and check largeLanguageModel.availability

--- Hiya/ContentView.swift
+++ Hiya/ContentView.swift
@@ -6,14 +6,26 @@
 //
 
 import SwiftUI
+import FoundationModels
 
 struct ContentView: View {
+    
+    private var largeLanguageModel = SystemLanguageModel.default
+    @State private var response = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            switch largeLanguageModel.availability {
+            case .available:
+                Text("Available")
+            case .unavailable(.deviceNotEligible):
+                Text("Your device is not eligible for Apple Intelligence.")
+            case .unavailable(.appleIntelligenceNotEnabled):
+                Text("Please enable Apple Intelligence in Settings.")
+            case .unavailable(.modelNotReady):
+                Text("AI model is not ready.")
+            case .unavailable:
+                Text("Unavailable")
+            }
         }
         .padding()
     }
